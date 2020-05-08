ccc0c4b0860465370ebaf57a0f5baada77b7b751
ssetiawan <Knightknight90>
2020-05-08 12:01:07 -0400

    commit

---
+++ src/main/java/com/example/SpringSimpleDemo/SpringSimpleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringSimpleDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSimpleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSimpleDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringSimpleDemo/SpringSimpleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringSimpleDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSimpleDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
