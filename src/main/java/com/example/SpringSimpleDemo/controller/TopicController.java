63892892836665acaec06ecfecf4d9ca823518aa
ssetiawan <Knightknight90>
2020-05-08 13:30:58 -0400

    another commit

---
+++ src/main/java/com/example/SpringSimpleDemo/controller/TopicController.java
@@ -0,0 +1,28 @@
+package com.example.SpringSimpleDemo.controller;
+
+import com.example.SpringSimpleDemo.model.Topic;
+import com.example.SpringSimpleDemo.service.TopicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class TopicController {
+
+    @Autowired
+    private TopicService topicService;
+
+    @RequestMapping("/topics")
+    public List<Topic> getAllTopics(){
+        return topicService.getAllTopics();
+    }
+
+    @RequestMapping("/topics/{id}")
+    public Topic getTopic(@PathVariable String id) {
+        return topicService.getTopic(id);
+    }
+}
